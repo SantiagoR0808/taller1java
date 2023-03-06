a0102476d6650345b485b998792de0687e8977f1
Santiago <eduardalmirante@outlook.es>
2023-03-05 20:50:50 -0500

    retoUno terminado

--- srodriguez_java/src/retos/retoUno.java
+++ srodriguez_java/src/retos/retoUno.java
@@ -1,6 +1,16 @@
-package retos
+package retos;
+import java.util.Scanner;
 
+public class retoUno {
+    public static void main(String[] args) {
+        Scanner pc = new Scanner(System.in);
+        float c,f;
 
-public static void main(String[] args) {
-    
+        System.out.println("Ingrese los grados F° que quiera conventir a C°");
+        f = pc.nextFloat();
+        c = (f-32) * 5/9 ;
+        System.out.println("En grados C serian:" +c );
+        
+pc.close();
+    }
 }
